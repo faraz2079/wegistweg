25b53ed5457142639bf07544551e762db37ef073
Adam Kwoska <a.kwoska@stud.fh-dortmund.de>
2024-11-09 20:33:39 +0100

    Add initial spring boot backend
    Contains some Repositories and Entites needed for the marketing functionalities.
    Also contains an example REST Controller.

---
+++ wegistweg-backend/src/main/java/de/fhdo/wegistweg/backend/domain/ProductDisplaySettings.java
@@ -0,0 +1,49 @@
+package de.fhdo.wegistweg.backend.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ProductDisplaySettings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    private boolean displayStockLevel;
+    private boolean displayViews;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public boolean isDisplayStockLevel() {
+        return displayStockLevel;
+    }
+
+    public void setDisplayStockLevel(boolean displayStockLevel) {
+        this.displayStockLevel = displayStockLevel;
+    }
+
+    public boolean isDisplayViews() {
+        return displayViews;
+    }
+
+    public void setDisplayViews(boolean displayViews) {
+        this.displayViews = displayViews;
+    }
+}
---
+++ wegistweg-backend/src/main/java/de/fhdo/wegistweg/backend/restapi/UserRestController.java
@@ -0,0 +1,32 @@
+package de.fhdo.wegistweg.backend.restapi;
+
+import de.fhdo.wegistweg.backend.domain.User;
+import de.fhdo.wegistweg.backend.services.UserService;
+import jakarta.annotation.PostConstruct;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/user", produces = "application/json")
+public class UserRestController {
+    private static Log log = LogFactory.getLog(UserRestController.class);
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/list")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
