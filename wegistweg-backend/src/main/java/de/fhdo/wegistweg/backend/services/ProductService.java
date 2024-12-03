8b20f8faba3d6168279e58782637b73bdbebabd3
Adam Kwoska <a.kwoska@stud.fh-dortmund.de>
2024-12-03 18:03:21 +0100

    GraphQl: Simple Product API

--- wegistweg-backend/src/main/java/de/fhdo/wegistweg/backend/services/ProductService.java
+++ wegistweg-backend/src/main/java/de/fhdo/wegistweg/backend/services/ProductService.java
@@ -2,6 +2,9 @@ package de.fhdo.wegistweg.backend.services;
 
 import de.fhdo.wegistweg.backend.domain.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     int getCurrentStock(Product product);
 }
