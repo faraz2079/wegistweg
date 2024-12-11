d6f5935986ccc99170d971caaa3e5cf6947efb7e
Adam Kwoska <a.kwoska@stud.fh-dortmund.de>
2024-12-11 16:42:36 +0100

    Base Vue.js Frontend

---
+++ frontend-vue/src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import CountDisplay from "@/components/CountDisplay.vue";
+
+export default {
+  components: {CountDisplay},
+  data() {
+    return {
+      count: 0
+    }
+  },
+
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    console.log('Component mounted.');
+    helper();
+  }
+}
+
+let helper = function () {
+  console.log('helper.')
+}
+</script>
+
+<template>
+  <main>
+    Count: {{ count }}
+
+    <br />
+
+    <button @click="increment">Increase count ({{ count }})</button>
+
+    <br />
+    <CountDisplay :count-prop="count"></CountDisplay>
+  </main>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
