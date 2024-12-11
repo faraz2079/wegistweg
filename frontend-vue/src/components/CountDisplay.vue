66e8f05f3d6325b854dbfe684a10f9dc477f7eb9
Adam Kwoska <a.kwoska@stud.fh-dortmund.de>
2024-12-11 16:42:36 +0100

    Base Vue.js Frontend

---
+++ frontend-vue/src/components/CountDisplay.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "CountDisplay",
+  props: ["countProp"]
+}
+</script>
+
+<template>
+  <button disabled>Count (component): {{ countProp }}</button>
+</template>
+
+<style scoped>
+
+</style>
