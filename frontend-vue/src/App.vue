1f59ff1f4471e1e42999fc53c55921f52f122ace
Adam Kwoska <a.kwoska@stud.fh-dortmund.de>
2024-12-31 16:28:36 +0100

    Frontend Vue: Add router and basic template

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,45 +1,51 @@
 <script>
-import CountDisplay from "@/components/CountDisplay.vue";
-
 export default {
-  components: {CountDisplay},
-  data() {
-    return {
-      count: 0
-    }
-  },
-
-  methods: {
-    increment() {
-      this.count++
-    }
-  },
-  mounted() {
-    console.log('Component mounted.');
-    helper();
-  }
-}
-
-let helper = function () {
-  console.log('helper.')
 }
 </script>
 
 <template>
+  <header>
+    <div class="logo">Wegistweg</div>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/testCounter">Sign Up</router-link></li>
+      </ul>
+    </nav>
+  </header>
+
   <main>
-    Count: {{ count }}
-
-    <br />
-
-    <button @click="increment">Increase count ({{ count }})</button>
-
-    <br />
-    <CountDisplay :count-prop="count"></CountDisplay>
+      <router-view></router-view>
   </main>
+
+  <footer>
+    <div class="container-horizontal">
+      <div class="footer-section">
+        <h2>Products</h2>
+        <ul>
+          <li><router-link to="/testCounter">All products</router-link></li>
+          <li><router-link to="/testCounter">Most viewed</router-link></li>
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h2>Account</h2>
+        <ul>
+          <li><router-link to="/testCounter">My Account</router-link></li>
+          <li><router-link to="/testCounter">Login / Register</router-link></li>
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h2>Contact</h2>
+        <ul>
+          <li><a href="#">faraz@mail</a></li>
+          <li><a href="#">adam@mail</a></li>
+        </ul>
+      </div>
+    </div>
+    <p class="copyright">&copy; The Springers 2024</p>
+  </footer>
 </template>
 
 <style scoped>
-button {
-  font-weight: bold;
-}
+
 </style>
---
+++ frontend-vue/src/components/TestCounter.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "CountDisplay",
+  props: ["countProp"],
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    console.log('Component mounted.');
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h2>Test Counter</h2>
+
+    Count: {{ count }}
+    <br />
+    <button @click="increment">Increase count to {{ count + 1 }}</button>
+    <br />
+    <router-link to="/test">Go to Home</router-link>
+  </div>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
